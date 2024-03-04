2fedf2cdd6414b0b85332d0e33b239cace26c115
ashkan <askanfotohi81@gmail.com>
2024-03-04 23:57:50 +0330

    start change status for mobile

--- src/app/layout/mianPage/category/category.component.ts
+++ src/app/layout/mianPage/category/category.component.ts
@@ -1,9 +1,12 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DividerModule} from "primeng/divider";
 import {CardComponent} from "../../../shared/component/card/card.component";
 import {Card} from "../../../shared/interface/card.interface";
 import {rout} from "../../../shared/model/routing.model";
 import {ButtonModule} from "primeng/button";
+import {ShareService} from "../../../shared/structure/share.service";
+import {NgForOf} from "@angular/common";
+import {filter} from "rxjs";
 
 @Component({
   selector: 'app-category',
@@ -11,31 +14,50 @@ import {ButtonModule} from "primeng/button";
   imports: [
     DividerModule,
     CardComponent,
-    ButtonModule
+    ButtonModule,
+    NgForOf
   ],
   templateUrl: './category.component.html',
   styleUrl: './category.component.css'
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit {
+  constructor(private share: ShareService) {
+  }
+
+  selectedCategory = <any>[""]
   cards: Card[] = [
     {
       isShow: true,
       router: rout.Main,
-      img:'../assets/img/JavaScript-Tutorial.svg',
-       description: '    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, iste laudantium non\n' +
+      img: '../assets/img/JavaScript-Tutorial.svg',
+      description: '    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, iste laudantium non\n' +
         '      odit perferendis quas quos sit? Adipisci autem debitis deserunt dicta error exercitationem, quasi suscipit?\n'
     }, {
-       isShow: true,
-      img:'../assets/img/typescript.svg',
+      isShow: true,
+      img: '../assets/img/typescript.svg',
       router: rout.Main,
       description: '    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, iste laudantium non\n' +
         '      odit perferendis quas quos sit? Adipisci autem debitis deserunt dicta error exercitationem, quasi suscipit?\n'
     }, {
-       isShow: true,
-      img:'../assets/img/Angular_Logo_SVG.svg',
+      isShow: true,
+      img: '../assets/img/Angular_Logo_SVG.svg',
       router: rout.Main,
       description: '    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, iste laudantium non\n' +
         '      odit perferendis quas quos sit? Adipisci autem debitis deserunt dicta error exercitationem, quasi suscipit?\n'
     }
   ];
+
+  ngOnInit(): void {
+    this.share._category.pipe(
+      filter(x => !this.selectedCategory.includes(x))
+    ).subscribe(res => {
+      this.selectedCategory.push(res)
+      console.log(res)
+    })
+
+  }
+
+  addCategory(event: string) {
+    this.share._category.next(event)
+  }
 }
