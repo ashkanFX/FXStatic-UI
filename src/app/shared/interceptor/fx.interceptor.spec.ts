d403ddf08675c300a52506d31ded5508804e28de
ashkan <askanfotohi81@gmail.com>
2024-07-26 12:13:14 +0330

    add interceptor

---
+++ src/app/shared/interceptor/fx.interceptor.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpInterceptorFn } from '@angular/common/http';
+
+import { fXInterceptor } from './fx.interceptor';
+
+describe('fXInterceptor', () => {
+  const interceptor: HttpInterceptorFn = (req, next) => 
+    TestBed.runInInjectionContext(() => fXInterceptor(req, next));
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+  });
+
+  it('should be created', () => {
+    expect(interceptor).toBeTruthy();
+  });
+});
---
+++ src/app/shared/interceptor/fx.interceptor.ts
@@ -0,0 +1,16 @@
+import {HttpInterceptorFn} from '@angular/common/http';
+
+export const fXInterceptor: HttpInterceptorFn = (req, next) => {
+  const authToken = 'YOUR_AUTH_TOKEN_HERE';
+  debugger
+  // Clone the request and add the authorization header
+  const authReq = req.clone({
+    setHeaders: {
+      Authorization: `Bearer ${authToken}`
+    }
+  });
+
+  // Pass the cloned request with the updated header to the next handler
+  return next(authReq);
+
+};
