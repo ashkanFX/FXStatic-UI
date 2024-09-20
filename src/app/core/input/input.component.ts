3cd45d6faaf9eeb9c91ae5a4618245248f8ebf3f
ashkan <ashkan.fotohi@gmail.com>
2024-09-20 17:39:31 +0330

    test commit

--- src/app/core/input/input.component.ts
+++ src/app/core/input/input.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [],
+  imports: [ ],
   templateUrl: './input.component.html',
   styleUrl: './input.component.css'
 })
 export class InputComponent {
-
+  constructor() {
+  }
 }
