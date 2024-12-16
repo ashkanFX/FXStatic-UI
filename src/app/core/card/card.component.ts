98baf199786a67664b4cecee8a898ef07da0e806
ashkan <ashkan.fotohi@gmail.com>
2024-12-16 21:26:48 +0330

    fix and show base data of post in fist page

--- src/app/core/card/card.component.ts
+++ src/app/core/card/card.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, ViewChild} from '@angular/core';
 import {CardModule} from "primeng/card";
 import {RouterLink} from "@angular/router";
 import {CommonModule, NgIf} from "@angular/common";
@@ -8,19 +8,28 @@ import {AvatarModule} from "primeng/avatar";
 @Component({
   selector: 'app-card',
   standalone: true,
-    imports: [
-        CardModule,
-        RouterLink,
-        CommonModule,
-        NgIf,
-        AvatarModule
-    ],
+  imports: [
+    CardModule,
+    RouterLink,
+    CommonModule,
+    NgIf,
+
+    AvatarModule
+  ],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
-export class CardComponent {
+export class CardComponent implements AfterViewInit {
+  @ViewChild('context') context: ElementRef
+  @Input() config: Card
+
   constructor() {
   }
 
-  @Input() config: Card
+  ngAfterViewInit() {
+    // Access the native element
+    console.log(this.context.nativeElement);
+    this.context.nativeElement.innerHTML = this.config.description;
+  }
+
 }
--- src/app/shared/interface/card.interface.ts
+++ src/app/shared/interface/card.interface.ts
@@ -1,11 +1,13 @@
 import {Routing} from "./routing.interface";
+import {UserResDto} from "../../routes/admin/user/user.res.dto";
 
-export interface Card{
+export interface Card {
   img?: string,
   title?: string,
   subText?: string,
   router?: Routing
   description?: string,
   isShow?: boolean;
+  user?: UserResDto;
   haveProfile: boolean;
 }
